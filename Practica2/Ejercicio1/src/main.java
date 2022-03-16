37666a4cf91220429b2f1832b4eb0f336c99b117
samu <samu13lop@gmail.com>
2022-03-16 14:30:54 +0100

    Ejercicio 1 Practica 2

---
+++ Practica2/Ejercicio1/src/main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce una cadena de mas de 7 caracteres: ");
+        String cadena = teclado.nextLine();
+
+        System.out.println("");
+        System.out.println("Longitud de la cadena : " + cadena.length());
+        if(cadena.length()>6)
+            System.out.printf("El caracter de la posicion 7 es: "+cadena.charAt(6));
+        int posicionX = cadena.indexOf('x');
+        if (posicionX!=-1)
+            System.out.println("La primera posicion en la que aparece la X es : "
+            + (posicionX+1));
+        else
+            System.out.println("La cadena no contiene ninguna X");
+        System.out.println("Cadena transformada en mayusculas :"+cadena.toUpperCase());
+
+    }
+}
